28bfb65468d0bef75e09689593901e00fbe47a47
NaTaLiaMoSKV <110030591+NaTaLiaMoSKV@users.noreply.github.com>
2022-10-11 17:22:04 +0000

    Deploying to gh-pages from @ NaTaLiaMoSKV/ice-cream@736ce95256b071f1bf684219bae8a7d8dc9e17ef 🚀

---
+++ main.c8e7142a.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".slider ").slick({slidesToShow:1,slidesToScroll:1,autoplay:!0,autoplaySpeed:3e3,arrows:!1,dots:!0,fade:!0,pauseOnHover:!1,responsive:[{breakpoint:768,settings:{slidesToShow:1}},{breakpoint:520,settings:{slidesToShow:1}}]})}));
+//# sourceMappingURL=main.c8e7142a.js.map
